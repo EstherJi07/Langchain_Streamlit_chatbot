be1a465e78b9a57c0554eeecd10d2a026c75344f
EstherJi07 <haestherji@gmail.com>
2025-01-09 17:20:33 +0900

    Update streamlit_rag_memory_r.py

--- streamlit_rag_memory_r.py
+++ streamlit_rag_memory_r.py
@@ -5,7 +5,7 @@ import tempfile
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.output_parsers import StrOutputParser
 
